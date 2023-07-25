ed8cfae5f4a57ecdab828beb2f0c66b000e546f1
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-25 16:59:38 +0200

    w trakcie...

---
+++ 5. Count of positives (slash) sum of negatives.py
@@ -0,0 +1,63 @@
+#Zadanie
+"""
+Given an array of integers.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+If the input is an empty array or is null, return an empty array.
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+"""
+#Mój kod
+print("-------------------------------------------------------------------------------")
+def count_positives_sum_negatives(arr):
+    lista = arr
+    lista.append(1234555556)
+    return lista
+#Test
+
+def proba():
+    test.assert_equals(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65])
+    test.assert_equals(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]),[8,-50])
+    test.assert_equals(count_positives_sum_negatives([1]),[1,0])
+    test.assert_equals(count_positives_sum_negatives([-1]),[0,-1])
+    test.assert_equals(count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]),[0,0])
+    test.assert_equals(count_positives_sum_negatives([]),[])
+
+def czarymary(): #Tutaj tworzymy konstrujcę funkcji do wywowłania w oparciu o "Sample Tests"
+    worklist = []
+    import inspect
+    cos = inspect.getsource(proba).splitlines()
+    for i in range(1, len(cos)):
+        newi = cos[i].strip().lstrip(f"test.assert_equals(")
+        end_index = newi.find(")")
+        result = newi[:end_index + 1]
+        worklist.append(result)
+    worklist2 = []
+    for i in worklist:
+        nameof = i.split("(")[0]
+        evali = eval(i)
+        magic_combination = [nameof, evali]
+        worklist2.append(magic_combination)
+    for i in range(len(worklist2)):
+        part1 = (worklist2[i][0])
+        part2 = (worklist2[i][1])
+        print("To jest finally", globals()[part1](part2))
+czarymary()
+
+"""
+def basic_test_cases():
+    count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65]
+basic_test_cases()
+"""
+"""
+def assert_equals():
+    return assert_equals
+def basic_test_cases():
+    assert_equals(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65])
+    assert_equals(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]),[8,-50])
+    assert_equals(count_positives_sum_negatives([1]),[1,0])
+    assert_equals(count_positives_sum_negatives([-1]),[0,-1])
+    assert_equals(count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]),[0,0])
+    assert_equals(count_positives_sum_negatives([]),[])
+print(basic_test_cases())
+#Najlepsze rozwiązanie od Codewars
+"""
