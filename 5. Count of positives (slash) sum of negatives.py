4ba6d94e8bea281862565b2d101372d95f7c8a96
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-25 18:51:26 +0200

    Update 5. Count of positives (slash) sum of negatives.py

--- 5. Count of positives (slash) sum of negatives.py
+++ 5. Count of positives (slash) sum of negatives.py
@@ -7,25 +7,34 @@ Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 """
 #Mój kod
-print("-------------------------------------------------------------------------------")
 def count_positives_sum_negatives(arr):
-    lista = arr
-    lista.append(1234555556)
-    return lista
-#Test
-
-def proba():
+    if arr == []:
+        return []
+    else:
+        positive_count = 0
+        negative_count = 0
+        for i in arr:
+            if i > 0:
+                positive_count += 1
+            elif i < 0:
+                negative_count += i
+            else:
+                None
+    return[positive_count, negative_count]
+#Tester (wersja 2)
+#półkopia z Codewars basic_test_cases
+def basic_test_cases():
     test.assert_equals(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65])
     test.assert_equals(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]),[8,-50])
     test.assert_equals(count_positives_sum_negatives([1]),[1,0])
     test.assert_equals(count_positives_sum_negatives([-1]),[0,-1])
     test.assert_equals(count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]),[0,0])
     test.assert_equals(count_positives_sum_negatives([]),[])
-
-def czarymary(): #Tutaj tworzymy konstrujcę funkcji do wywowłania w oparciu o "Sample Tests"
+#implementacja samorobnego testera
+def codewars_sample_tests_workaround_implementation():
     worklist = []
     import inspect
-    cos = inspect.getsource(proba).splitlines()
+    cos = inspect.getsource(basic_test_cases).splitlines()
     for i in range(1, len(cos)):
         newi = cos[i].strip().lstrip(f"test.assert_equals(")
         end_index = newi.find(")")
@@ -35,23 +44,4 @@ def czarymary(): #Tutaj tworzymy konstrujcę funkcji do wywowłania w oparciu o 
     for i in worklist:
         evali = eval(i)
         print(evali)
-czarymary()
-
-"""
-def basic_test_cases():
-    count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65]
-basic_test_cases()
-"""
-"""
-def assert_equals():
-    return assert_equals
-def basic_test_cases():
-    assert_equals(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10,-65])
-    assert_equals(count_positives_sum_negatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]),[8,-50])
-    assert_equals(count_positives_sum_negatives([1]),[1,0])
-    assert_equals(count_positives_sum_negatives([-1]),[0,-1])
-    assert_equals(count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]),[0,0])
-    assert_equals(count_positives_sum_negatives([]),[])
-print(basic_test_cases())
-#Najlepsze rozwiązanie od Codewars
-"""
+codewars_sample_tests_workaround_implementation()
