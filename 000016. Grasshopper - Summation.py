9b78f7e585a1e4600c54555b518ec498cab3be20
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-26 23:45:48 +0200

    completed

---
+++ 000016. Grasshopper - Summation.py
@@ -0,0 +1,60 @@
+#Zadanie
+"""
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+"""
+#Mój kod
+def summation(num):
+    incr_list = []
+    for i in range(1, num+1): incr_list.append(i)
+    return sum(incr_list)
+#Najlepsze rozwiązanie od Codewars
+def summation(num):
+    return sum(range(num + 1))
+########################################################################################################################
+#Tester (wersja 2d "Definder" - rewizja w kata nr 7 (wersja b), rewizja w kata nr 11 (wersja c),
+# rewizja w kata nr 12 (wersja d "Definder"))
+#półkopia z Codewars basic_test_cases
+def basic_test_cases():
+    test.assert_equals(summation(1), 1)
+    test.assert_equals(summation(8), 36)
+    test.assert_equals(summation(22), 253)
+    test.assert_equals(summation(100), 5050)
+    test.assert_equals(summation(213), 22791)
+#implementacja samorobnego testera
+def codewars_sample_tests_workaround_implementation():
+    def definder():#poszukuje nazwy wszystkich definicji w aktualnym pliku i namierza tą z listy
+        def tracker():
+            import sys
+            import inspect
+            import re
+            code = inspect.getsource(sys.modules[__name__])
+            finder = re.findall(r'\bdef\s+(\w+)\(', code)
+            return finder
+        def hunter():
+            hit_list = tracker()
+            words = ["basic_test_cases", "fixed_tests"]
+            for i in hit_list:
+                if i in words:
+                    return i
+                else: pass
+            print(f"definition not implemented in tester")
+        return hunter()
+    word = globals()[definder()]
+    worklist = []
+    import inspect
+    cos = inspect.getsource(word).splitlines()
+    for i in range(1, len(cos)):
+        newi = cos[i].strip().lstrip(str("test.assert_equals")).lstrip("(")
+        end_index = newi.rfind(")",0,newi.__len__()-1)
+        result = newi[:end_index + 1]
+        worklist.append(result)
+    worklist2 = []
+    for i in worklist:
+        evali = eval(i)
+        print(evali)
+codewars_sample_tests_workaround_implementation()
