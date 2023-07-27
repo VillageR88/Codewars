b22a0661ce5096d769429040cc50f934061b8108
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-27 21:08:11 +0200

    completed

---
+++ 000023. Sum of odd numbers.py
@@ -0,0 +1,42 @@
+##Zadanie
+"""
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+"""
+
+##Solution
+#Moje
+def row_sum_odd_numbers(n):
+    value = -1
+    for i in range(1, n+1):
+        total = 0
+        for i in range(i):
+            value += 2
+            total += value
+    return total
+#Najlepsze rozwiązanie od Codewars
+def row_sum_odd_numbers(n):
+    return n ** 3
+##Tester - codewars_test framework z pominiętym import solution, który jest zawarty w kodzie
+import codewars_test as test
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(row_sum_odd_numbers(1), 1)
+        test.assert_equals(row_sum_odd_numbers(2), 8)
+        test.assert_equals(row_sum_odd_numbers(13), 2197)
+        test.assert_equals(row_sum_odd_numbers(19), 6859)
+        test.assert_equals(row_sum_odd_numbers(41), 68921)
