580c7b6e0381eee12d6971d2b7f6f04b74fba1bf
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-28 10:27:45 +0200

    completed

---
+++ 000025. Sum of positive.py
@@ -0,0 +1,37 @@
+##Zadanie
+"""
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+"""
+##Solution
+#Moje
+def positive_sum(arr):
+    positvie_numbers_count = 0
+    for i in arr:
+        if i > 0:
+            positvie_numbers_count += i
+    return positvie_numbers_count
+#Najlepsze rozwiązanie od Codewars
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+##Tester - codewars_test framework z pominiętym import solution, który jest zawarty w kodzie
+import codewars_test as test
+
+@test.describe("positive_sum")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(positive_sum([1,2,3,4,5]),15)
+        test.assert_equals(positive_sum([1,-2,3,4,5]),13)
+        test.assert_equals(positive_sum([-1,2,3,4,-5]),9)
+        
+    @test.it("returns 0 when array is empty")
+    def empty_case():
+        test.assert_equals(positive_sum([]),0)      
+        
+    @test.it("returns 0 when all elements are negative")
+    def negative_case():
+        test.assert_equals(positive_sum([-1,-2,-3,-4,-5]),0)
