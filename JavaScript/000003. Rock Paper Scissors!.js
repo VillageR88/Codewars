94412b185f032631802475a6ead7655bde32ca64
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-29 23:15:53 +0200

    completed

--- JavaScript/000003. Rock Paper Scissors!.js
+++ JavaScript/000003. Rock Paper Scissors!.js
@@ -12,10 +12,25 @@ Examples(Input1, Input2 --> Output):
 */
 ///Solution
 //Moje
+
 const rps = (p1, p2) => {
+  if ((p1 == "rock" && p2 == "scissors")||(p1 == "paper" && p2 == "rock")||(p1 == "scissors" && p2 == "paper")) return "Player 1 won!";
+  else if ((p1 == "scissors" && p2 == "rock") ||(p1 === "rock" && p2 === "paper")||(p1 === "paper" && p2 === "scissors")) return "Player 2 won!";
+  else if (p1 === p2) return "Draw!";
 };
 //Najlepsze rozwiązanie od Codewars
-
+/*
+const rps = (p1, p2) => {
+  if (p1 === p2) return "Draw!";
+  var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+  if (p2 === rules[p1]) {
+    return "Player 1 won!";
+  }
+  else {
+    return "Player 2 won!";
+  }
+};
+*/
 ///Tester
 //JS 'Codewars' 'Sample Test' (JSCSTT) Tester v0.1.0 by VR. - prototyp
 const Test = require('@codewars/test-compat');
