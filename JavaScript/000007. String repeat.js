85f3a26b087f77ba9ba9696ded4169b8c20b4f30
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-30 14:40:45 +0200

    completed

---
+++ JavaScript/000007. String repeat.js
@@ -0,0 +1,28 @@
+/*Zadanie
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+Examples (input -> output)
+
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+*/
+///Solution
+//Moje
+function repeatStr (n, s) {
+    let word = ""; for (i = 1; i <= n; i++) word += s; return word;
+}
+//Najlepsze rozwiązanie od Codewars
+/*
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+*/
+///Tester
+//JS 'Codewars' 'Sample Test' (JSCSTT) Tester v(nazwa pliku) by VR.
+//const Test = require('@codewars/test-compat'); *przechowalnia
+//const getMsg = (n) => `Player ${n} won!`; *przechowalnia
+const chai = require("chai");
+const assert = chai.assert;
+assert.strictEqual(repeatStr(3, "*"), "***");
+assert.strictEqual(repeatStr(5, "#"), "#####");
+assert.strictEqual(repeatStr(2, "ha "), "ha ha ");
