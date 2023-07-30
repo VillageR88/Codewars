98cc1502972907f39bc488df82075f989c3c5ca3
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-30 23:26:16 +0200

    c

---
+++ JavaScript/000019. Convert a Boolean to a String.js
@@ -0,0 +1,33 @@
+/*Zadanie
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+*/
+///Solution
+//Moje
+function booleanToString(b){
+  return b.toString()
+}
+//Najlepsze rozwiązanie od Codewars
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester (JSCSTT) v0.1.0 by VR.
+//Deklaracje i definicje - część dostosowana do VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+function describe (opis, funkcja) {
+  funkcja();
+};
+function it (opis, funkcja) {
+  funkcja();
+};
+//Describe - część niezmieniana
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+Test.assertEquals(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+  });
+});
