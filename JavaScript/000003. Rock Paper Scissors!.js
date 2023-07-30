04dad2bbff60c550fb950d0c91e3aa6b4574f29e
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-30 09:34:23 +0200

    completed

--- JavaScript/000003. Rock Paper Scissors!.js
+++ JavaScript/000003. Rock Paper Scissors!.js
@@ -12,18 +12,17 @@ Examples(Input1, Input2 --> Output):
 */
 ///Solution
 //Moje
-
 const rps = (p1, p2) => {
   if ((p1 == "rock" && p2 == "scissors")||(p1 == "paper" && p2 == "rock")||(p1 == "scissors" && p2 == "paper")) return "Player 1 won!";
   else if ((p1 == "scissors" && p2 == "rock") ||(p1 === "rock" && p2 === "paper")||(p1 === "paper" && p2 === "scissors")) return "Player 2 won!";
   else if (p1 === p2) return "Draw!";
 };
-//Najlepsze rozwiązanie od Codewars
+//Najlepsze rozwiązanie od Codewars *mod
 /*
 const rps = (p1, p2) => {
   if (p1 === p2) return "Draw!";
   var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
-  if (p2 === rules[p1]) {
+  if (rules[p1] === p2) {
     return "Player 1 won!";
   }
   else {
