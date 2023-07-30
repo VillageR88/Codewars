332c0560ccdaac6aba611167bea00ce6b8f84617
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-30 14:52:14 +0200

    completed

---
+++ JavaScript/000008. Fake Binary.js
@@ -0,0 +1,24 @@
+/*Zadanie
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+*/
+///Solution
+//Moje
+function fakeBin(x){let replaced = ""; for (i of x) if (i < 5) replaced += 0; else replaced +=1; return replaced;}
+//Najlepsze rozwiązanie od Codewars
+/*
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+*/
+///Tester
+//JS 'Codewars' 'Sample Test' (JSCSTT) Tester v(nazwa pliku) by VR.
+//const Test = require('@codewars/test-compat'); *przechowalnia
+//const getMsg = (n) => `Player ${n} won!`; *przechowalnia
+const chai = require("chai");
+const assert = chai.assert;
+assert.strictEqual(fakeBin('45385593107843568'), '01011110001100111');
+assert.strictEqual(fakeBin('509321967506747'), '101000111101101'); 
+assert.strictEqual(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
