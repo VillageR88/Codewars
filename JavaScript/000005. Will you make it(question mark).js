9c08a40fa4b0ed4a317e8f92b86e417958ba7592
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-30 13:18:09 +0200

    completed

---
+++ JavaScript/000005. Will you make it(question mark).js
@@ -0,0 +1,29 @@
+/*Zadanie
+You were camping with your friends far away from home, but when it's time to go back,
+you realize that your fuel is running out and the nearest pump is 50 miles away!
+You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not.
+
+*/
+///Solution
+//Moje
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  if ((mpg*fuelLeft)-distanceToPump>=0) return true; else return false;
+};
+//Najlepsze rozwiązanie od Codewars
+/*
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return distanceToPump/mpg <= fuelLeft
+};
+*/
+///Tester
+//JS 'Codewars' 'Sample Test' (JSCSTT) Tester v(nazwa pliku) by VR.
+//const Test = require('@codewars/test-compat'); *przechowalnia
+//const getMsg = (n) => `Player ${n} won!`; *przechowalnia
+const chai = require("chai");
+const assert = chai.assert;
+assert.equal(zeroFuel(50, 25, 2), true);
+assert.equal(zeroFuel(100, 50, 1), false);
