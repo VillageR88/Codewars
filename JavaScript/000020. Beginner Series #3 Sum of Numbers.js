109bbbe590b33867d2652d5fba8c012e11d4b1af
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-30 23:48:05 +0200

    c

---
+++ JavaScript/000020. Beginner Series #3 Sum of Numbers.js
@@ -0,0 +1,49 @@
+/*Zadanie
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+Examples (a, b) --> output (explanation)
+
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+Your function should only return a number, not the explanation about how you get that number.
+
+*/
+///Solution
+//Moje
+function getSum(a, b)
+{
+  let calc = 0;
+  if (a<b) for (i = a; i<=b ; i++) calc += i;
+  else if (a>b) for (i = a; i>=b ; i--) calc += i;
+  else calc = a
+  return calc
+}
+//Najlepsze rozwiązanie od Codewars
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester (JSCSTT) v0.1.0 by VR.
+//Deklaracje i definicje - część dostosowana do VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+function describe (opis, funkcja) {
+  funkcja();
+};
+function it (opis, funkcja) {
+  funkcja();
+};
+//Describe - część niezmieniana
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {         
+    assert.strictEqual(getSum(0,-1), -1);
+    assert.strictEqual(getSum(0, 1),  1);
+    assert.strictEqual(getSum(2, 2),  2);
+  })
+});
