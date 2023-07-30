f0e2165838978a9c7c13613c396a742cfa22023a
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-07-30 20:58:03 +0200

    wip

--- JavaScript/000017. MakeUpperCase.js
+++ JavaScript/000017. MakeUpperCase.js
@@ -1,17 +1,11 @@
 /*Zadanie
-Very simple, given an integer or a floating-point number, find its opposite.
-
-Examples:
-
-1: -1
-14: -14
--34: 34
+Write a function which converts the input string to uppercase.
 
 */
 ///Solution
 //Moje
-function opposite(number) {
-  return -number
+function makeUpperCase(str) {
+  // Code here
 }
 //Najlepsze rozwiązanie od Codewars
 /*
@@ -29,26 +23,11 @@ function it (opis, funkcja) {
   funkcja();
 };
 //Describe
-describe('Fixed tests', () => {
-  it('Is it a function?', () => {
-    assert.strictEqual(typeof opposite, 'function', 'opposite should be a function');
-  });
-  it('should return -1', () => {
-    assert.strictEqual(opposite(1), -1);
-  });
-  it('should return 0', () => {
-    assert.strictEqual(opposite(0), 0);
-  });
-  it('should return -4.25', () => {
-    assert.strictEqual(opposite(4.25), -4.25);
-  });
-  it('should return -3.3333333', () => {
-    assert.strictEqual(opposite(3.3333333), -3.3333333);
-  });
-  it('should return 12525220.3325', () => {
-    assert.strictEqual(opposite(-12525220.3325), 12525220.3325);
-  });
-  it('should return 5', () => {
-    assert.strictEqual(opposite(-5), 5);
+describe("Basic Tests", function(){
+  it("should pass the basic tests", function(){
+    assert.strictEqual(makeUpperCase(""),           "");
+    assert.strictEqual(makeUpperCase("hello"), "HELLO");
+    assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+    assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
   });
 });
