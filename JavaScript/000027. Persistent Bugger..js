4570931856f36aa5a07fcaa77c4b7a5a71bbeb72
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-02 19:18:58 +0200

    completed

---
+++ JavaScript/000027. Persistent Bugger..js
@@ -0,0 +1,64 @@
+/*Zadanie
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
+
+*/
+///Solution
+//Moje
+function persistence(num) {
+  var counter = 0;
+  function inner_persistence(num) {
+    if (num.toString().length > 1) {
+      counter++;
+    }
+    var sum = 1;
+    num = num.toString();
+    for (i of num) {
+      sum *= Number(i);
+    }
+    if (sum.toString().length > 1) {
+      return inner_persistence(sum);
+    }
+    return counter;
+  }
+  return inner_persistence(num);
+}
+//Najlepsze rozwiązanie od Codewars
+/*
+var maxSequence = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
+}
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester (JSCSTT) v0.1.0 by VR.
+//Deklaracje i definicje - część dostosowana do VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+function describe (opis, funkcja) {
+  funkcja();
+};
+function it (opis, funkcja) {
+  funkcja();
+};
+//Describe - część niezmieniana
+describe("Persistent Bugger.", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(persistence(39),3);
+    assert.strictEqual(persistence(4),0);
+    assert.strictEqual(persistence(25),2);
+    assert.strictEqual(persistence(999),4);
+  });
+});
