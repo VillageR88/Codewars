b6c292e6a6a5e94b7e4b54522d33662d791b1804
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-03 17:56:03 +0200

    completed

---
+++ TypeScript/000003. Switch it Up!.ts
@@ -0,0 +1,46 @@
+/*Task
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+
+*/
+///Solution
+//My
+export function switchItUp(intNumber:number):string {
+  return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][intNumber];
+;}
+//Codewars recommended
+/*
+export function switchItUp(intNumber:number):string {
+   enum out{ Zero,One,Two,Three,Four,Five,Six,Seven,Eight,Nine,Ten};
+   return out[intNumber];
+}
+*/
+///Tester => TS 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+//const Test = require('@codewars/test-compat');
+//const chai = require("chai");
+//const assert = chai.assert;
+//chai.config.truncateThreshold=0;
+function describe (opis: any, func: any) {
+  func();
+};
+function it (opis: any, func: any) {
+  func();
+};
+var solution = {switchItUp}
+//Describe - invariant part
+import { assert } from "chai";
+//import solution = require('@codewars/test-compat'););
+
+describe("solution", function(){
+  it("Basic Tests", function(){
+    assert.equal(solution.switchItUp(1),"One");
+    assert.equal(solution.switchItUp(3),"Three");
+    assert.equal(solution.switchItUp(5),"Five");
+  });
+});
