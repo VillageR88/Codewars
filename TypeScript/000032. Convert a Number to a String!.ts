0d21bcc5b6232fdf41c6cf69a56e23d8c9f62090
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-05 19:30:37 +0200

    Create 000032. Convert a Number to a String!.ts

---
+++ TypeScript/000032. Convert a Number to a String!.ts
@@ -0,0 +1,37 @@
+///Task
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+Examples (input --> output):
+
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+
+*/
+///Solution
+//My
+export function numberToString(num: number): string {
+  return num.toString();
+}
+//Codewars recommended
+/*
+ */
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (opis: any, func: any) {
+  func();
+};
+function it (opis: any, func: any) {
+func();
+};
+import { assert } from "chai";
+import { expect } from "chai";
+//Describe - invariant part
+describe("numberToString", function() {
+  it("should work correctly", () => {
+    expect(numberToString(67)).to.equal('67');
+  });
+});
