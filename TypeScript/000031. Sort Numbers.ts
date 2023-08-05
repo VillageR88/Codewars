b99e639da3148255742efa59f60a7d611beae28e
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-05 19:25:27 +0200

    Create 000031. Sort Numbers.ts

---
+++ TypeScript/000031. Sort Numbers.ts
@@ -0,0 +1,36 @@
+///Task
+/*
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1, 2, 5, 10, 50]
+solution([]); // should return []
+
+*/
+///Solution
+//My
+export function solution(nums: number[]): number[] {
+  return nums.sort((a, b) => a - b);
+}
+//Codewars recommended
+/*
+ */
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (opis: any, func: any) {
+  func();
+};
+function it (opis: any, func: any) {
+func();
+};
+import { assert } from "chai";
+solution.solution = solution;
+//Describe - invariant part
+describe("Sample Test Cases", function(){
+  it("Should return an array", function() {
+    assert.deepEqual(solution.solution([1, 2, 10, 50, 5]), [1,2,5,10,50]);
+    assert.deepEqual(solution.solution([]), []);
+  });
+});
