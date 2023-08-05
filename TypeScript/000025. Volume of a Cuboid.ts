9231eba06ced7c25beec34a51747d6644303545b
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-05 14:20:21 +0200

    Create 000025. Volume of a Cuboid.ts

---
+++ TypeScript/000025. Volume of a Cuboid.ts
@@ -0,0 +1,33 @@
+///Task
+/*
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+*/
+///Solution
+//My
+export function getVolumeOfCuboid(length: number, width:number, height:number): number {
+ return length*width*height;
+}
+//Codewars recommended
+/*
+ */
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (opis: any, func: any) {
+  func();
+};
+function it (opis: any, func: any) {
+func();
+};
+import { assert } from "chai";
+var solution = {getVolumeOfCuboid};
+//Describe - invariant part
+describe("Some testing", function() {
+  it("Sample tests", function() {
+    assert.equal(solution.getVolumeOfCuboid(1,2,2), 4);    
+    assert.equal(solution.getVolumeOfCuboid(6.3,2,5), 63);    
+    assert.equal(solution.getVolumeOfCuboid(1,1,1), 1);  
+    assert.equal(solution.getVolumeOfCuboid(52,17,5), 4420);  
+  });
+});
