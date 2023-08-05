477eae1d0ff01d24c474672251e24717f6eaad8a
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-05 19:05:50 +0200

    Create 000029. Thinkful - Logic Drills(colon) Traffic light.ts

---
+++ TypeScript/000029. Thinkful - Logic Drills(colon) Traffic light.ts
@@ -0,0 +1,35 @@
+///Task
+/*
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+
+*/
+///Solution
+//My
+export function updateLight(current: string): string {
+  return {"green" : "yellow", "yellow" : "red", "red" : "green"}[current as keyof void];
+}
+//Codewars recommended
+/*
+ */
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (opis: any, func: any) {
+  func();
+};
+function it (opis: any, func: any) {
+func();
+};
+import { assert } from "chai";
+//Describe - invariant part
+describe("Thinkful - Logic Drills: Traffic light", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(updateLight("green"), "yellow");
+    assert.strictEqual(updateLight("yellow"), "red");
+    assert.strictEqual(updateLight("red"), "green");
+  });
+});
