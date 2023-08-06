e53e1d730d3ef2fcf6a8da79e6873eafb04d299c
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-06 20:18:23 +0200

    Create 000037. Is it a palindrome(question).ts

---
+++ TypeScript/000037. Is it a palindrome(question).ts
@@ -0,0 +1,32 @@
+///Task
+/*
+Write a function that checks if a given string (case insensitive) is a palindrome. A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
+
+*/
+///Solution
+//My
+export function isPalindrome(x: string): boolean {
+  console.log();
+  return [...x].toString().toLowerCase() == [...x].reverse().toString().toLowerCase();
+}
+//Codewars recommended
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (opis: any, func: any) {func();};
+function it (opis: any, func: any) {func();};
+import { assert } from "chai";
+//Describe - invariant part
+
+describe("Basic tests", function() {
+  it("Testing for 'a'", () => assert.strictEqual(isPalindrome("a"), true));
+  it("Testing for 'aba'", () => assert.strictEqual(isPalindrome("aba"), true));
+  it("Testing for 'Abba'", () => assert.strictEqual(isPalindrome("Abba"), true));
+  it("Testing for 'hello'", () => assert.strictEqual(isPalindrome("hello"), false));
+  it("Testing for 'Bob'", () => assert.strictEqual(isPalindrome("Bob"), true));
+  it("Testing for 'Madam'", () => assert.strictEqual(isPalindrome("Madam"), true));
+  it("Testing for 'AbBa'", () => assert.strictEqual(isPalindrome("AbBa"), true));
+  it("Testing for ''", () => assert.strictEqual(isPalindrome(""), true));
+});
