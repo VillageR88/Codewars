4ac1663d3414f382278153c83d63e4bcf79fff74
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-07 22:21:18 +0200

    Create 000052. Who likes it(question).ts

---
+++ TypeScript/000052. Who likes it(question).ts
@@ -0,0 +1,42 @@
+///Task
+/*
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+*/
+///Solution
+//My soloution
+export const likes = (a : string[]) : string => {
+  return a.length>=4?a[0]+", "+a[1]+" and "+(a.length-2)+" others like this":
+  a.length==3?a[0]+", "+a[1]+" and "+a[2]+" like this":
+  a.length==2?a[0]+" and "+a[1]+" like this":
+  a.length==1?a[0]+" likes this":"no one likes this";
+}
+//Codewars recommended
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_opis: any, func: any) {func();};
+function it (_opis: any, func: any) {func();};
+import { assert } from "chai";
+//Describe - invariant part
+describe('static tests', function() {
+  it('should return correct text', function() {
+    assert.equal(likes([]), 'no one likes this');
+    assert.equal(likes(['Peter']), 'Peter likes this');
+    assert.equal(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+    assert.equal(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+    assert.equal(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+  });
+});
