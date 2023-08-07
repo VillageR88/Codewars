7d66a407753731199ec1ef740cc52417cc124e6c
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-07 21:52:14 +0200

    Create 000051. Odd or Even(question).ts

---
+++ TypeScript/000051. Odd or Even(question).ts
@@ -0,0 +1,69 @@
+///Task
+/*
+ask:
+
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+Examples:
+
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+
+Have fun!
+
+*/
+///Solution
+//My soloution
+export function oddOrEven(array: number[]):any {
+  return array.length==0?"even":array.reduce((a,b)=>a+b)%2?"odd":"even";
+}
+//Codewars recommended
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_opis: any, func: any) {func();};
+function it (_opis: any, func: any) {func();};
+import { assert } from "chai";
+//Describe - invariant part
+describe('Fixed tests', () => {
+  it('Edge tests', () => {
+    assert.equal(oddOrEven([0]), 'even')
+    assert.equal(oddOrEven([1]), 'odd')
+    assert.equal(oddOrEven([]), 'even')
+  });
+  
+  it('Even tests', () => {
+    assert.equal(oddOrEven([0, 1, 5]), 'even')
+    assert.equal(oddOrEven([0, 1, 3]), 'even')
+    assert.equal(oddOrEven([1023, 1, 2]), 'even')
+  });
+  
+  it('Negative Even tests', () => {
+    assert.equal(oddOrEven([0, -1, -5]), 'even')
+    assert.equal(oddOrEven([0, -1, -3]), 'even')
+    assert.equal(oddOrEven([-1023, 1, -2]), 'even')
+  });
+  
+  it('Odd tests', () => {
+    assert.equal(oddOrEven([0, 1, 2]), 'odd')
+    assert.equal(oddOrEven([0, 1, 4]), 'odd')
+    assert.equal(oddOrEven([1023, 1, 3]), 'odd')
+  });
+  
+  it('Negative Odd tests', () => {
+    assert.equal(oddOrEven([0, -1, 2]), 'odd')
+    assert.equal(oddOrEven([0, 1, -4]), 'odd')
+    assert.equal(oddOrEven([-1023, -1, 3]), 'odd')
+  });
+});
