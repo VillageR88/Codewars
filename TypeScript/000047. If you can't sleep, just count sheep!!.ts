d3d4f2dae0f07db6703c5f71ad0d2aba2689e91d
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-07 17:39:41 +0200

    Create 000047. If you can't sleep, just count sheep!!.ts

---
+++ TypeScript/000047. If you can't sleep, just count sheep!!.ts
@@ -0,0 +1,32 @@
+///Task
+/*
+If you can't sleep, just count sheep!!
+Task:
+
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+*/
+///Solution
+//My
+export function countSheep(num: number): string {
+  return Array(num).fill(0).map((x,index)=>x=(index+1).toString()+" sheep...").join("").toString();
+}
+//Codewars recommended
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_opis: any, func: any) {func();};
+function it (_opis: any, func: any) {func();};
+import { assert } from "chai";
+var solution = {countSheep};
+//Describe - invariant part
+describe("Sample Test Cases", function(){
+  it("Should return a string", function() {
+    assert.equal(solution.countSheep(0), '');
+    assert.equal(solution.countSheep(1), '1 sheep...');
+    assert.equal(solution.countSheep(2), '1 sheep...2 sheep...');
+    assert.equal(solution.countSheep(3), '1 sheep...2 sheep...3 sheep...');
+  });
+});
