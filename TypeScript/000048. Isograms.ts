2f2588a2aba2b5dd72c586a0524abad7918603a5
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-07 18:00:05 +0200

    Create 000048. Isograms.ts

---
+++ TypeScript/000048. Isograms.ts
@@ -0,0 +1,41 @@
+///Task
+/*
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false
+
+*/
+///Solution
+//My
+export function isIsogram(str: string): boolean{
+  return Array.from(new Set(str.toLocaleLowerCase())).join("") == str.toLowerCase();
+}
+//Codewars recommended
+/*
+export function isIsogram (str: string): boolean {
+  return (new Set(str.toLowerCase())).size === str.length
+
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_opis: any, func: any) {func();};
+function it (_opis: any, func: any) {func();};
+import { assert } from "chai";
+//Describe - invariant part
+describe("example", function() {
+  it("test", function() {
+    assert.equal( isIsogram("Dermatoglyphics"), true );
+    assert.equal( isIsogram("isogram"), true );
+    assert.equal( isIsogram("aba"), false, "same chars may not be adjacent" );
+    assert.equal( isIsogram("moOse"), false, "same chars may not be same case" );
+    assert.equal( isIsogram("isIsogram"), false );
+    assert.equal( isIsogram(""), true, "an empty string is a valid isogram" );
+  });
+});
