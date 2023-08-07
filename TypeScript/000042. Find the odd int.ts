ef7c77d9c1e414693f1e321a540b61bc61ee42d2
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-07 02:50:49 +0200

    Create 000042. Find the odd int.ts

---
+++ TypeScript/000042. Find the odd int.ts
@@ -0,0 +1,58 @@
+///Task
+/*
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+Examples
+
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+*/
+///Solution
+//My
+export const findOdd = (xs: number[]): number => {
+  for (var i of xs) {
+    var counter = 0;
+    for (var j of xs) {
+      if (i==j) {
+        counter++;
+      }
+    }
+    if (counter % 2 != 0) return i;
+  }
+  return 0;
+};
+
+
+//Codewars recommended
+/*
+export const findOdd = (xs: number[]): number => {
+  // happy coding!
+  return xs.reduce( (a,b)=> a^b);
+}
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_opis: any, func: any) {func();};
+function it (_opis: any, func: any) {func();};
+import { assert } from "chai";
+//Describe - invariant part
+describe('Example tests', function() {
+  doTest([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5], 5);
+  doTest([1,1,2,-2,5,2,4,4,-1,-2,5], -1);
+  doTest([20,1,1,2,2,3,3,5,5,4,20,4,5], 5);
+  doTest([10], 10);
+  doTest([1,1,1,1,1,1,10,1,1,1,1], 10);
+  doTest([5,4,3,2,1,5,4,3,2,10,10], 1);
+});
+
+function doTest(a: number[], n: number) {
+  it(`xs = ${JSON.stringify(a)} ; n = ${n}`, () => {
+    assert.strictEqual(findOdd(a), n);
+  });
+}
