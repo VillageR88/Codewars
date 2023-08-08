210ed07d52bbba7c6c65117756b7076d552d608b
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-08 20:15:27 +0200

    Create 000055. Beginner Series #2 Clock.ts

---
+++ TypeScript/000055. Beginner Series #2 Clock.ts
@@ -0,0 +1,45 @@
+///Task
+/*
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+Example:
+
+h = 0
+m = 1
+s = 1
+
+result = 61000
+
+Input constraints:
+
+    0 <= h <= 23
+    0 <= m <= 59
+    0 <= s <= 59
+
+
+*/
+///Solution
+//My soloution
+export function past(h: number, m: number, s: number): number {
+  return h*3600000+m*60000+s*1000;
+}
+//Codewars recommended
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_opis: any, func: any) {func();};
+function it (_opis: any, func: any) {func();};
+import { assert } from "chai";
+//Describe - invariant part
+describe("Fixed Tests", () => {
+  it("Tests", () => {
+    assert.equal(past(0, 1, 1), 61000);
+    assert.equal(past(1, 1, 1), 3661000);
+    assert.equal(past(0, 0, 0), 0);
+    assert.equal(past(1, 0, 1), 3601000);
+    assert.equal(past(1, 0, 0), 3600000);
+  });
+});
