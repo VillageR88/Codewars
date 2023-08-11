ce387097466bf66d2aa844867fd827c58e83758e
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-11 17:31:50 +0200

    Create 000061. Filter out the geese.ts

---
+++ TypeScript/000061. Filter out the geese.ts
@@ -0,0 +1,51 @@
+///Task
+/*
+Write a function that takes a list of strings as an argument and returns a filtered list
+containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+For example, if this array were passed as an argument:
+
+ ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+The elements in the returned array should be in the same order as in the initial array passed to your function,
+albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided,
+and some elements may be repeated.
+
+*/
+///Solution
+//My soloution
+export function gooseFilter (birds: string[]): string[] {
+  const geese: string[] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((x) => !geese.includes(x));
+}
+//Codewars recommended
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_desc: any, func: any) {func();};
+function it (_desc: any, func: any) {func();};
+import { assert } from "chai";
+import { expect } from 'chai';
+//Describe - invariant part
+// TODO Add your tests here
+describe("Basic tests",function(){
+  it("Mixed list",function(){
+    assert.deepEqual(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]),["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
+  });
+  it("No geese",function(){
+    assert.deepEqual(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]),["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]);
+  });
+  it("All geese",function(){
+    assert.deepEqual(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]),[]);
+  });
+});
