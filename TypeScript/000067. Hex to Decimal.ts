c4cb33ac659bee6590a35546033400ace6a41b77
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-12 20:18:30 +0200

    Create 000067. Hex to Decimal.ts

---
+++ TypeScript/000067. Hex to Decimal.ts
@@ -0,0 +1,33 @@
+///Task
+/*
+Complete the function which converts hex number (given as a string) to a decimal number.
+
+*/
+///Solution
+//My soloution
+export function hexToDec(hexString:string):number {
+  return parseInt(hexString, 16);
+}
+//Codewars recommended
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_desc: any, func: any) {func();};
+function it (_desc: any, func: any) {func();};
+import { randomNumber } from "@codewars/test-compat";
+import { assert } from "chai";
+import { expect } from 'chai';
+import { teardown } from "mocha";
+var solution = {hexToDec}
+//Describe - invariant part
+describe("solution", function(){
+  it("Basic Tests", function(){
+    assert.equal(solution.hexToDec("1"), 1);
+    assert.equal(solution.hexToDec("a"), 10);
+    assert.equal(solution.hexToDec("10"), 16);
+    assert.equal(solution.hexToDec("FF"), 255);
+    assert.equal(solution.hexToDec("-C"), -12);
+  });
+});
