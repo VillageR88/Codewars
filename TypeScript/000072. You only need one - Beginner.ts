e5b7bf277d0889401b8984f6832de013e62cbde0
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-13 23:51:48 +0200

    Create 000072. You only need one - Beginner.ts

---
+++ TypeScript/000072. You only need one - Beginner.ts
@@ -0,0 +1,33 @@
+///Task
+/*
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+*/
+///Solution
+//My soloution
+export const check = (a: (number | string)[], x: number | string): boolean => a.includes(x);
+//Codewars recommended)
+/*
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_desc: any, func: any) {func();};
+function it (_desc: any, func: any) {func();};
+import { randomNumber } from "@codewars/test-compat";
+import { assert } from "chai";
+import { expect } from 'chai';
+import { teardown } from "mocha";
+//Describe - invariant part
+describe("example", function() {
+  it("test", function() {
+    assert.strictEqual(check([66, 101], 66), true);
+    assert.strictEqual(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+    assert.strictEqual(check(['t', 'e', 's', 't'], 'e'), true);
+    assert.strictEqual(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+  });
+});
