7a76782041dc7a69924cdd73d029b1a91b3177b2
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-14 00:13:46 +0200

    Create 000074. Reverse List Order.ts

---
+++ TypeScript/000074. Reverse List Order.ts
@@ -0,0 +1,34 @@
+///Task
+/*
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+Examples (Input -> Output)
+
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+*/
+///Solution
+//My soloution
+export function reverseList(list: number[]): number[] {
+  return list.reverse();
+}
+//Codewars recommended)
+/*
+export const reverseList = (list: number[]): number[] => [...list].reverse();
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_desc: any, func: any) {func();};
+function it (_desc: any, func: any) {func();};
+import { randomNumber } from "@codewars/test-compat";
+import { assert } from "chai";
+import { expect } from 'chai';
+import { teardown } from "mocha";
+//Describe - invariant part
+describe("reverseList", function(){
+  it("should reverse some sample arrays", function(){
+    assert.deepEqual(reverseList([1,2,3,4]), [4,3,2,1], "Input=[1,2,3,4]");
+    assert.deepEqual(reverseList([3,1,5,4]), [4,5,1,3], "Input=[3,1,5,4]");
+  });
+});
