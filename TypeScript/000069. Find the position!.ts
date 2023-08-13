31c2da7571927bc290c2c303273516b3c16fdeea
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-13 20:21:24 +0200

    Create 000069. Find the position!.ts

---
+++ TypeScript/000069. Find the position!.ts
@@ -0,0 +1,35 @@
+///Task
+/*
+When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: 1"
+
+*/
+///Solution
+//My soloution
+export function position(alphabet:string):string {
+  return ("Position of alphabet: " + String(alphabet.charCodeAt(0) - 96));
+}
+//Codewars recommended)
+/*
+export const position = (alphabet: any): string => `Position of alphabet: ${alphabet.charCodeAt() - 96}`;
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_desc: any, func: any) {func();};
+function it (_desc: any, func: any) {func();};
+import { randomNumber } from "@codewars/test-compat";
+import { assert } from "chai";
+import { expect } from 'chai';
+import { teardown } from "mocha";
+//Describe - invariant part
+describe("solution", function(){
+  it("Basic tests", function(){
+    assert.strictEqual(position("a"),"Position of alphabet: 1");
+    assert.strictEqual(position("z"),"Position of alphabet: 26");
+    assert.strictEqual(position("e"),"Position of alphabet: 5");
+  });  
+});
