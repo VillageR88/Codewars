79d7b6ea72f3fbdc2a01d24b18513b30daa9bf85
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-15 17:37:34 +0200

    Create 000078. Find the capitals.ts

---
+++ TypeScript/000078. Find the capitals.ts
@@ -0,0 +1,34 @@
+///Task
+/*
+Instructions
+
+Write a function that takes a single string (word) as argument.
+The function must return an ordered list containing the indexes of all capital letters in the string.
+Example (Input --> Output)
+
+"CodEWaRs" --> [0,3,4,6]
+
+*/
+///Solution
+//My soloution
+export function capitals (word: string): number[] {
+  return Array.from([...word].map((a) => word.match(/[A-Z]/g)?.includes(a) ? [...word].indexOf(a) : "").filter((x) => x !== "")) as number[];
+}
+//Codewars recommended)
+/*  
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_desc: any, func: any) {func();};
+function it (_desc: any, func: any) {func();};
+import { randomNumber } from "@codewars/test-compat";
+import { assert } from "chai";
+import { expect } from 'chai';
+import { teardown } from "mocha";
+//Describe - invariant part
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(capitals('CodEWaRs'), [0,3,4,6] );
+  });
+});
