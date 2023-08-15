8b733f56f1bfbb445bd73f4ab22d2acc4868d925
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-15 18:59:28 +0200

    Create 000079. Get the Middle Character.ts

---
+++ TypeScript/000079. Get the Middle Character.ts
@@ -0,0 +1,50 @@
+///Task
+/*
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+
+*/
+///Solution
+//My soloution
+export function getMiddle(s:string) {
+  return s.slice(s.length / 2 - (s.length % 2 ^ 1), s.length / 2 + 1);
+}
+//Codewars recommended)
+/*  
+*/
+
+///Tester => TrueScript 'Codewars' 'Sample Test' Tester (TSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+function describe (_desc: any, func: any) {func();};
+function it (_desc: any, func: any) {func();};
+import { randomNumber } from "@codewars/test-compat";
+import { assert } from "chai";
+import { expect } from 'chai';
+import { teardown } from "mocha";
+//Describe - invariant part
+function test(string:string, expected:string) {
+  assert.strictEqual(getMiddle(string), expected)
+}
+describe("solution", function(){
+  it("should handle basic tests", function() {
+    test("test", "es");
+    test("testing", "t");
+  });
+});
