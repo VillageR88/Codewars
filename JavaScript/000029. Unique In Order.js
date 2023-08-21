1e775cce0ed2415006fc06734924b79ef99bd7be
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-22 00:32:03 +0200

    Create 000029. Unique In Order.js

---
+++ JavaScript/000029. Unique In Order.js
@@ -0,0 +1,46 @@
+///Task
+/*
+
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items
+without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+*/
+///Solution
+//My
+var uniqueInOrder = function (iterable) {
+  return  [...iterable].filter((_, i, a) => a[i] != a[i - 1]);
+
+  //return [...iterable].includes(0|1|2|3|4|5|6|7|8|9) ? [iterable].filter((x, y) => x^y) : [...[...iterable].reduce((x, y, i, a) => `${x}` + (a[i - 1] == `${y}` ? "" : `${y}`))];
+  //return [...[...iterable].reduce((x, y, i, a) => `${x}` + (a[i - 1] == `${y}` ? "" : `${y}`))];
+}
+//Codewars recommended
+/*
+var uniqueInOrder=function(iterable){
+    return [...iterable].filter((a, i) => a !== iterable[i-1])
+}
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester (JSCSTT) v0.1.0 by VR.
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+function describe (opis, funkcja) {
+  funkcja();
+};
+function it (opis, funkcja) {
+  funkcja();
+};
+//Describe - invariant part
+describe("Tests", () => {
+  it("test", () => {
+    assert.deepEqual(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+    assert.deepEqual(uniqueInOrder([1,2,2,3,3]), [1,2,3])
+  });
+});
