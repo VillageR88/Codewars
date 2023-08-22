5b3dc62504fcd66047a8f4d8d91ee030ec62ccfd
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-22 20:24:25 +0200

    Update 000035. Two fighters, one winner..js

--- JavaScript/000035. Two fighters, one winner..js
+++ JavaScript/000035. Two fighters, one winner..js
@@ -48,18 +48,12 @@ function Fighter(name, health, damagePerAttack) {
 }
 describe("Two fighters, one winner.", () => {
   it("Example Test Cases", () => {
-    
+  
     assert.strictEqual(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"), "Lew");
-
     assert.strictEqual(declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry"), "Harry");
-
     assert.strictEqual(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"), "Harald")
-
     assert.strictEqual(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"), "Harald")
-
     assert.strictEqual(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry"), "Harald")
-
     assert.strictEqual(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald"), "Harald")
-
   });
 });
