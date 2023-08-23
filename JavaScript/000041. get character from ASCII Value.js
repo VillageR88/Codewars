aa9cc62bb22b00602938cd27efcba6bd63079ccf
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-23 16:19:59 +0200

    Update 000041. get character from ASCII Value.js

--- JavaScript/000041. get character from ASCII Value.js
+++ JavaScript/000041. get character from ASCII Value.js
@@ -12,11 +12,10 @@ For ASCII table, you can refer to http://www.asciitable.com/
 */
 ///Solution
 //My
-function getChar(c){
-  // ...
-}
+const getChar = (c) => String.fromCharCode(c);
 //Codewars recommended
 /*
+const getChar = String.fromCharCode;
 */
 ///Tester => JS 'Codewars' 'Sample Test' Tester
 //Declarations and definitions - part adjusted to VSC
@@ -32,17 +31,18 @@ function it (tag, func) {
   func();
 };
 //Describe - invariant part
-describe( "diamond()", function(){
-
-  it("Valid diamonds", () => {
-    assert.strictEqual(diamond(1), "*\n")
-    assert.strictEqual(diamond(3), " *\n***\n *\n")
-    assert.strictEqual(diamond(5), "  *\n ***\n*****\n ***\n  *\n")
-  });
-  
-  it("Invalid diamonds", () => {
-    assert.strictEqual(diamond(2), null)
-    assert.strictEqual(diamond(-3), null)
-    assert.strictEqual(diamond(0), null)
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(getChar(55),'7')
+Test.assertEquals(getChar(56),'8')
+Test.assertEquals(getChar(57),'9')
+Test.assertEquals(getChar(58),':')
+Test.assertEquals(getChar(59),';')
+Test.assertEquals(getChar(60),'<')
+Test.assertEquals(getChar(61),'=')
+Test.assertEquals(getChar(62),'>')
+Test.assertEquals(getChar(63),'?')
+Test.assertEquals(getChar(64),'@')
+Test.assertEquals(getChar(65),'A')
   });
 });
