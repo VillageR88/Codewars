bafacd25fa11d1305ab82df1ae91b5ed02801ec3
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-23 16:50:29 +0200

    Create 000043. Parse nice int from char problem.js

---
+++ JavaScript/000043. Parse nice int from char problem.js
@@ -0,0 +1,34 @@
+///Task
+/*
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+*/
+///Solution
+//My
+const getAge = (inputString) => +inputString.match(/[0-9]/);
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(getAge("4 years old"), 4);
+  })
+})
