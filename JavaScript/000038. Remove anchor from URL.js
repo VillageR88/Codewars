1d608e77ec21293c96fd80129fde46b7ab18e440
VillageR88 <139210946+VillageR88@users.noreply.github.com>
2023-08-23 08:31:46 +0200

    Create 000038. Remove anchor from URL.js

---
+++ JavaScript/000038. Remove anchor from URL.js
@@ -0,0 +1,39 @@
+///Task
+/*
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+Examples
+
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+*/
+///Solution
+//My
+const removeUrlAnchor = (url) => url.replace(url.match(/(#.+)/g) , "");
+//Codewars recommended
+/*
+function removeUrlAnchor(url){
+  return url.split('#')[0];
+}
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(removeUrlAnchor('www.codewars.com#about'), 'www.codewars.com')
+    assert.strictEqual(removeUrlAnchor('www.codewars.com/katas/?page=1#about'), 'www.codewars.com/katas/?page=1')
+    assert.strictEqual(removeUrlAnchor('www.codewars.com/katas/'), 'www.codewars.com/katas/')
+    })
+  })
