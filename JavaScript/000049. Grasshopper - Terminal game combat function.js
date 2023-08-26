fc3aa2b4ca3433f7f64e3fe8e6168e82de1ae029
VillageR88 <villager88pl@gmail.com>
2023-08-26 13:02:45 +0200

    commit -S

---
+++ JavaScript/000049. Grasshopper - Terminal game combat function.js
@@ -0,0 +1,32 @@
+///Task
+/*
+Create a combat function that takes the player's current health and the amount of damage recieved,
+and returns the player's new health. Health can't be less than 0.
+*/
+///Solution
+//My
+const combat = (h, d) => Math.max(0, h - d);
+//Codewars recommended
+/* 
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("The combat() function", function () {
+  it("should work for some example tests", function () {
+    Test.assertEquals(combat(100, 5), 95);
+    Test.assertEquals(combat(92, 8), 84);
+    Test.assertEquals(combat(20, 30), 0, "Health cannot go below 0");
+  });
+});
