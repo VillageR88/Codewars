63ec84c7facebc3012d40821397950859de7983d
VillageR88 <villager88pl@gmail.com>
2023-08-26 13:58:35 +0200

    commit

---
+++ JavaScript/000052. Drink about.js
@@ -0,0 +1,52 @@
+///Task
+/*
+Instructions
+Output
+
+    Kids drink toddy.
+    Teens drink coke.
+    Young adults drink beer.
+    Adults drink whisky.
+
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+    Children under 14 old.
+    Teens under 18 old.
+    Young under 21 old.
+    Adults have 21 or more.
+
+Examples: (Input --> Output)
+
+13 --> "drink toddy"
+17 --> "drink coke"
+18 --> "drink beer"
+20 --> "drink beer"
+30 --> "drink whisky"
+*/
+///Solution
+//My
+const peopleWithAgeDrink = (old) => "drink " + (old > 20 ? "whisky" : old > 17 ? "beer" : old > 13 ? "coke" : "toddy");
+//Codewars recommended
+/* 
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(peopleWithAgeDrink(22), 'drink whisky');
+    })
+  })
