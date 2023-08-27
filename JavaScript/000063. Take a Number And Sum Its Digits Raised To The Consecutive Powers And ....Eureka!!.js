283cbbd0ff5c4a282653371647e4980efef5ce5b
VillageR88 <villager88pl@gmail.com>
2023-08-27 12:09:29 +0200

    commit

---
+++ JavaScript/000063. Take a Number And Sum Its Digits Raised To The Consecutive Powers And ....Eureka!!.js
@@ -0,0 +1,53 @@
+///Task
+/*
+The number 898989 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. 
+What's the use of saying "Eureka"? Because this sum gives the same number: 89=81+9289 = 8^1 + 9^289=81+92
+
+The next number in having this property is 135135135:
+
+See this property again: 135=11+32+53135 = 1^1 + 3^2 + 5^3135=11+32+53
+Task
+
+We need a function to collect these numbers, that may receive two integers aaa, bbb that defines the range
+[a,b][a, b][a,b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+Examples
+
+Let's see some cases (input -> output):
+
+1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+
+If there are no numbers of this kind in the range [a,b][a, b][a,b] the function should output an empty list.
+
+90, 100 --> []
+*/
+///Solution
+//My
+const sumDigPow = (a, b) => [...Array(-a + b + 1)].map((_, index) => index + a).filter((x) => x == ("" + x).split("").reduce((a, b, i) => +a + b ** [i + 1]));
+//Codewars recommended
+/* 
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+describe("Tests", function() {
+  it("Fixed tests", function() {
+      assert.deepEqual(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+      assert.deepEqual(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89])
+      assert.deepEqual(sumDigPow(10, 100),  [89])
+      assert.deepEqual(sumDigPow(90, 100), [])
+      assert.deepEqual(sumDigPow(90, 150), [135])
+      assert.deepEqual(sumDigPow(50, 150), [89, 135])
+      assert.deepEqual(sumDigPow(10, 150), [89, 135])
+  });
+});
