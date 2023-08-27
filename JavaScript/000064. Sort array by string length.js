e78534dd94280df9f6739dbf7cde5ead8014c75a
VillageR88 <villager88pl@gmail.com>
2023-08-27 12:35:54 +0200

    commit

---
+++ JavaScript/000064. Sort array by string length.js
@@ -0,0 +1,45 @@
+///Task
+/*
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+For example, if this array were passed as an argument:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+*/
+///Solution
+//My
+const sortByLength = array => array.sort((a, b) => a.length - b.length);
+//Codewars recommended
+/* 
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+describe("Example tests",function(){
+  it("Test 1",function(){
+    Test.assertDeepEquals(sortByLength(["Beg", "Life", "I", "To"]),["I", "To", "Beg", "Life"]);
+  });
+  it("Test 2",function(){
+    Test.assertDeepEquals(sortByLength(["", "Moderately", "Brains", "Pizza"]),["", "Pizza", "Brains", "Moderately"]);
+  });
+  it("Test 3",function(){
+    Test.assertDeepEquals(sortByLength(["Longer", "Longest", "Short"]),["Short", "Longer", "Longest"]);
+  });
+});
