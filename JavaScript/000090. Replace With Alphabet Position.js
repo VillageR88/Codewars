3372a5820fb125122c1192199fb910c59ada0f6b
VillageR88 <villager88pl@gmail.com>
2023-08-30 18:08:33 +0200

    commit

---
+++ JavaScript/000090. Replace With Alphabet Position.js
@@ -0,0 +1,43 @@
+///Task
+/*
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+Example
+
+alphabetPosition("The sunset sets at twelve o' clock.")
+
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+*/
+///Solution
+//
+//My
+const alphabetPosition = text => [...text.toLowerCase()].map((x) => x.charCodeAt() - 96).filter((x) => x > 0 && x <= 26).join(" ");
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(alphabetPosition("The sunset sets at twelve o' clock."), "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11");
+Test.assertEquals(alphabetPosition("The narwhal bacons at midnight."), "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20");
+  });
+});
