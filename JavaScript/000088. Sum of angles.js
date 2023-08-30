8f5346a0e2522a8b64e8b3c4b68ecf067227706a
VillageR88 <villager88pl@gmail.com>
2023-08-30 17:43:48 +0200

    commit

---
+++ JavaScript/000088. Sum of angles.js
@@ -0,0 +1,32 @@
+///Task
+/*
+GFind the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+*/
+///Solution
+//
+//My
+const angle = n => n * 180 - 360;
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(angle(3), 180);
+    assert.strictEqual(angle(4), 360);
+  });
+})
