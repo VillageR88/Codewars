54cbd6c731de61c80280a22a4e918785a9e420a7
VillageR88 <villager88pl@gmail.com>
2023-08-30 22:01:17 +0200

    half done

---
+++ JavaScript/000094. Simple Encryption No1 - Alternating Split.js
@@ -0,0 +1,78 @@
+///Task
+/*
+Implement a pseudo-encryption algorithm which given a string S and an integer N concatenates all the odd-indexed characters of S with all the even-indexed characters of S, this process should be repeated N times.
+
+Examples:
+
+encrypt("012345", 1)  =>  "135024"
+encrypt("012345", 2)  =>  "135024"  ->  "304152"
+encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+encrypt("01234", 1)  =>  "13024"
+encrypt("01234", 2)  =>  "13024"  ->  "32104"
+encrypt("01234", 3)  =>  "13024"  ->  "32104"  ->  "20314"
+
+Together with the encryption function, you should also implement a decryption function which reverses the process.
+
+If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+*/
+///Solution
+//My
+// write the function isAnagram
+const encrypt = (text, n, temp_text = text) =>(
+  console.log(n, text) ,
+  (n > 0) ? (temp_text = [], [...text].map((x, i) => i % 2 != 0 && temp_text.push(x)) , [...text].map((x, i) => i % 2 == 0 && temp_text.push(x)) , (temp_text = temp_text.join(""), encrypt(text = temp_text, n -= 1, temp_text))) : temp_text
+)
+  function decrypt(encryptedText, n) {
+
+}
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Solution", function(){
+  it("EncryptExampleTests", function(){    
+    Test.assertEquals(encrypt("This is a test!", 0), "This is a test!");
+    Test.assertEquals(encrypt("This is a test!", 1), "hsi  etTi sats!");
+    Test.assertEquals(encrypt("This is a test!", 2), "s eT ashi tist!");
+    Test.assertEquals(encrypt("This is a test!", 3), " Tah itse sits!");
+    Test.assertEquals(encrypt("This is a test!", 4), "This is a test!");
+    Test.assertEquals(encrypt("This is a test!", -1), "This is a test!");
+    Test.assertEquals(encrypt("This kata is very interesting!", 1), "hskt svr neetn!Ti aai eyitrsig");
+  });
+});
+
+describe("Solution", function(){
+  it("DecryptExampleTests", function(){    
+    Test.assertEquals(decrypt("This is a test!", 0), "This is a test!");
+    Test.assertEquals(decrypt("hsi  etTi sats!", 1), "This is a test!");
+    Test.assertEquals(decrypt("s eT ashi tist!", 2), "This is a test!");
+    Test.assertEquals(decrypt(" Tah itse sits!", 3), "This is a test!");
+    Test.assertEquals(decrypt("This is a test!", 4), "This is a test!");
+    Test.assertEquals(decrypt("This is a test!", -1), "This is a test!");
+    Test.assertEquals(decrypt("hskt svr neetn!Ti aai eyitrsig", 1), "This kata is very interesting!");
+  });
+});
+
+describe("Solution", function(){
+  it("Null or Empty", function(){    
+    Test.assertEquals(encrypt("", 0), "");
+    Test.assertEquals(decrypt("", 0), "");
+    Test.assertEquals(encrypt(null, 0), null);
+    Test.assertEquals(decrypt(null, 0), null);
+  });
+});
