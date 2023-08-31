56313ae5adb77ba778b49cd2e6a8d0eb6bbe5139
VillageR88 <villager88pl@gmail.com>
2023-08-31 22:20:26 +0200

    WIP1

---
+++ JavaScript/000097. Extract the domain name from a URL.js
@@ -0,0 +1,42 @@
+///Task
+/*
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+*/
+///Solution
+//My
+// write the function isAnagram
+function domainName(url){
+  console.log(url);
+  return url.match(/\w+(?=\.org)|(?=w)|((?<=w\.)|(?<=\/\/))\S+?(?=\.)||(?<=org)/g).join("");
+  }
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Sample test", () => {
+  it("Should pass sample tests", () => {
+    assert.equal(domainName("http://google.com"), "google");
+    assert.equal(domainName("http://google.co.jp"), "google");
+    assert.equal(domainName("www.xakep.ru"), "xakep");
+    assert.equal(domainName("https://youtube.com"), "youtube");
+    assert.equal(domainName("https://hyphen-site.org"), "hyphen-site");
+  })  
+})
