078cba2817853d6a5a839b745726e419f5ad8e52
VillageR88 <villager88pl@gmail.com>
2023-09-01 09:16:29 +0200

    commit

---
+++ JavaScript/000098. Grasshopper - Terminal game move function.js
@@ -0,0 +1,51 @@
+///Task
+/*
+Terminal game move function
+
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+Example:
+
+move(3, 6) should equal 15
+*/
+///Solution
+//My
+// write the function isAnagram
+const move = (p, r) => p + 2 * r;
+//Codewars recommended
+/*
+function domainName(url){
+  return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+}
+OR
+function domainName(url){  
+  return url.replace(/.+\/\/|www.|\..+/g, '')
+}
+OR
+function domainName(url){
+  return url.replace(/(https?:\/\/)?(www\.)?/, '').split('.')[0]
+}
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe('move function', () => {
+  it('basic tests', () => {
+    Test.assertEquals(move(0, 4), 8);
+    Test.assertEquals(move(3, 6), 15);
+    Test.assertEquals(move(2, 5), 12);
+  })
+})
