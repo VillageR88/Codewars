f7880dccf5bdd5705b50e4f1fbac46b01bd8efda
VillageR88 <villager88pl@gmail.com>
2023-09-01 12:37:26 +0200

    commit

---
+++ JavaScript/000102. String incrementer.js
@@ -0,0 +1,68 @@
+///Task
+/*
+Your job is to write a function which increments a string, to create a new string.
+
+    If the string already ends with a number, the number should be incremented by 1.
+    If the string does not end with a number. the number 1 should be appended to the new string.
+
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.
+*/
+///Solution
+//My
+//\d+(?!\S)
+function incrementString (s, p0, p1) {
+  try {p0 = s.match(/.+(?<!\d)/g)[0];} catch (error) {p0 = "";}
+  try {p1 = s.match(/\d+$/g)[0];} catch (error) {p1 = "x"; return p0 + 1;}
+  return p0 + p1.slice(0, - ("" + (+ p1 + 1)).length) + (+ p1 + 1);
+}
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Tests suite", () => {
+
+  const { assert } = require('chai');
+
+  function doTest(input, expected) {
+    const actual = incrementString(input);
+    assert.strictEqual(actual, expected, `for string: "${input}"\n`);
+  }
+
+  it("sample tests", () => {
+    doTest("foobar000", "foobar001");
+    doTest("foobar999", "foobar1000");
+    doTest("foobar00999", "foobar01000");
+    doTest("foo", "foo1");
+    doTest("foobar001", "foobar002");
+    doTest("foobar1", "foobar2");
+    doTest("1", "2");
+    doTest("009", "010");
+    doTest("fo99obar99", "fo99obar100");
+  });
+});
