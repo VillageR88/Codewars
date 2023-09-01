a1eb667ebda62512b2be5db4e1c55fca73e0bab9
VillageR88 <villager88pl@gmail.com>
2023-09-01 17:48:27 +0200

    comit

---
+++ JavaScript/000107. Enumerable Magic No 25 - Take the First N Elements.js
@@ -0,0 +1,35 @@
+///Task
+/*
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+If you need help, here's a reference:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+*/
+///Solution
+//My
+const take = (a, n) => a.slice(0, n);
+//Codewars recommended
+/*
+const stringClean = s => s.replace(/\d/g, "");
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Sample Tests", function(){
+  it("should work for sample tests", function(){
+    Test.assertDeepEquals(take([0, 1, 2, 3, 5, 8, 13], 3), [0, 1, 2], "should return the first 3 items");
+  });
+});
