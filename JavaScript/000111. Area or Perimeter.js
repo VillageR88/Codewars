adc6c508ba2e5dba24740987867254a89e52850b
VillageR88 <villager88pl@gmail.com>
2023-09-01 19:59:52 +0200

    commit

---
+++ JavaScript/000111. Area or Perimeter.js
@@ -0,0 +1,41 @@
+///Task
+/*
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+*/
+///Solution
+//My
+const areaOrPerimeter = (l, w) => l == w ? l * w : (l + w) * 2;
+//Codewars recommended
+/*
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+}*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const assert = chai.assert;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold=0;
+function describe (tag, func) {
+  func();
+};
+function it (tag, func) {
+  func();
+};
+//Describe - invariant part
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(areaOrPerimeter(3,  3),  9);
+    assert.strictEqual(areaOrPerimeter(6, 10), 32);
+  });
+});
