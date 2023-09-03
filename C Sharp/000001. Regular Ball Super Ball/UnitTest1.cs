f6d8d912e44e0ae7310a78362b23533cb742f8cd
VillageR88 <villager88pl@gmail.com>
2023-09-03 20:41:44 +0200

    commit - WiP

--- C Sharp/000001. Regular Ball Super Ball/UnitTest1.cs
+++ C Sharp/000001. Regular Ball Super Ball/UnitTest1.cs
@@ -1,6 +1,19 @@
+///Task
+/*
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+///Solution
+//My
 using System;
 using NUnit.Framework;
-
 
 public class Ball {
   public string ballType { get; set; }
@@ -9,8 +22,10 @@ public class Ball {
 
   }
 }
-
-
+//Codewars recommended
+/*
+*/
+///Tester => C# 'Codewars' 'Sample Tests' Tester
 [TestFixture]
 public class FactorialTests {
   [Test]
