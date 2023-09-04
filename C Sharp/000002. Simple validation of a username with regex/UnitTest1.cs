8fc1ef43c3b6dcab043afe8800df8d02ad0cc42c
VillageR88 <villager88pl@gmail.com>
2023-09-04 11:05:24 +0200

    commit

---
+++ C Sharp/000002. Simple validation of a username with regex/UnitTest1.cs
@@ -0,0 +1,47 @@
+///Task
+/*
+Write a simple regex to validate a username. Allowed characters are:
+
+    lowercase letters,
+    numbers,
+    underscore
+
+Length should be between 4 and 16 characters (both included).
+*/
+///Tester => C# 'Codewars' 'Sample Tests' Tester - part 1 (invariant) directives
+using NUnit.Framework;
+///Solution
+//My
+using System;
+using System.Text.RegularExpressions;
+
+public class Kata
+{
+  public static bool ValidateUsr(string username) 
+  {
+    if (username.Length >= 4 && username.Length <= 16 && username.ToLower() == username && !username.Contains(" ")) return true;
+    else return false;
+  }
+}
+//Codewars recommended
+/*
+*/
+///Tester => C# 'Codewars' 'Sample Tests' Tester - part 2 (adjusted) sample tests
+public class Tests
+{
+  [Test]
+  [TestCase("asddsa", ExpectedResult=true)]
+  [TestCase("a", ExpectedResult=false)]
+  [TestCase("Hass", ExpectedResult=false)]
+  [TestCase("Hasd_12ssssssssssssssasasasasasssasassss", ExpectedResult=false)]
+  [TestCase("", ExpectedResult=false)]
+  [TestCase("____", ExpectedResult=true)]
+  [TestCase("012", ExpectedResult=false)]
+  [TestCase("p1pp1", ExpectedResult=true)]
+  [TestCase("asd43 34", ExpectedResult=false)]
+  [TestCase("asd43_34", ExpectedResult=true)]
+  public static bool FixedTest(string username)
+  {
+    return Kata.ValidateUsr(username);
+  }
+}
