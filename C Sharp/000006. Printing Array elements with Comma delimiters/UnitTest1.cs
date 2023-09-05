23001e6a58a983060d7fb95f3946aebbe15abc1d
VillageR88 <villager88pl@gmail.com>
2023-09-05 09:44:31 +0200

    commit

---
+++ C Sharp/000006. Printing Array elements with Comma delimiters/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace _000006._Printing_Array_elements_with_Comma_delimiters;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
