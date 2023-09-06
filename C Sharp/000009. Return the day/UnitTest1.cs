254fb7d6f3cb9110aeff73f7acf2fb8cd820e037
VillageR88 <villager88pl@gmail.com>
2023-09-06 15:58:36 +0200

    commit

--- C Sharp/000009. Return the day/UnitTest1.cs
+++ C Sharp/000009. Return the day/UnitTest1.cs
@@ -40,6 +40,31 @@ public class Kata
 
 //Codewars recommended
 /*
+public class Kata
+{
+    public static string WhatDay(int n)
+    {
+        switch (n)
+        {
+            case 1:
+                return "Sunday";
+            case 2:
+                return "Monday";
+            case 3:
+                return "Tuesday";
+            case 4:
+                return "Wednesday";
+            case 5:
+                return "Thursday";
+            case 6:
+                return "Friday";
+            case 7:
+                return "Saturday";
+            default:
+                return "Wrong, please enter a number between 1 and 7";
+        }
+    }
+}
 */
 /// Tester => C# 'Codewars' 'Sample Tests' Tester - part 2 (adjusted) sample tests
 namespace Solution
