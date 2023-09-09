36dde9903b84cf7d1247d5f325dcfb6961bf9311
VillageR88 <villager88pl@gmail.com>
2023-09-09 13:37:45 +0200

    commit

---
+++ JavaScript/000115. Write Number in Expanded Form.js
@@ -0,0 +1,42 @@
+///Task
+/*
+Write Number in Expanded Form
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+
+NOTE: All numbers will be whole numbers greater than 0.
+*/
+///Solution
+//My
+//
+const expandedForm = n => [..."" + n].map((x, i) => x != 0 && x + "0".repeat(("" + n).length - 1 - i)).filter(x => x != 0).join(" + ");
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant partdescribe("Fixed Tests", function () {
+const { assert } = require('chai');
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(expandedForm(12), '10 + 2');
+    assert.strictEqual(expandedForm(42), '40 + 2');
+    assert.strictEqual(expandedForm(70304), '70000 + 300 + 4');
+  });
+});
