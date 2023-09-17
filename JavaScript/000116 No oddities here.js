ba7275f87b6458b1d8925458124d7a8de2226a76
VillageR88 <villager88pl@gmail.com>
2023-09-17 21:19:12 +0200

    commit

---
+++ JavaScript/000116 No oddities here.js
@@ -0,0 +1,35 @@
+///Task
+/*
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
+*/
+///Solution
+//My
+const noOdds = (values) => values.filter(x => !(x % 2));
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant partdescribe("Fixed Tests", function () {
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(noOdds([0, 1]), [0])
+    assert.deepEqual(noOdds([0, 1, 2, 3]), [0, 2])
+  })
+})
