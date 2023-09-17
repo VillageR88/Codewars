c5ded8cb5242698059e2bb7a705f4d59e4bb4f3b
VillageR88 <villager88pl@gmail.com>
2023-09-17 21:31:17 +0200

    commit

---
+++ JavaScript/000117. Hello, Name or World!.js
@@ -0,0 +1,44 @@
+///Task
+/*
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+Examples:
+
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given 
+  or `name` = ""        => return "Hello, World!"
+*/
+///Solution
+//My
+const hello = name => name == undefined || name == '' ? "Hello, World!" : `Hello, ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`;
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant partdescribe("Fixed Tests", function () {
+const assert = require('chai').assert;
+describe("Sample Tests", () => {
+  it("should work for sample inputs", () => {
+    let actualJohn = hello('johN');
+    assert.isDefined(actualJohn, "You have to return a string instead of logging it to the console");
+    assert.strictEqual(actualJohn, 'Hello, John!', "returns 'Hello, John!' when given 'johN'");
+    assert.strictEqual(hello('alice'), 'Hello, Alice!', "returns 'Hello, Alice!' when given 'alice'");
+    assert.strictEqual(hello(), 'Hello, World!', "returns 'Hello, World!' when name is not given");
+    assert.strictEqual(hello(''), 'Hello, World!', "returns 'Hello, World!' when name is an empty string");
+  });
+});  
