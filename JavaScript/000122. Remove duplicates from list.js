8d3251fdeef925291213803579b6d5c17ff5d25a
VillageR88 <villager88pl@gmail.com>
2023-09-18 16:56:28 +0200

    commit

---
+++ JavaScript/000122. Remove duplicates from list.js
@@ -0,0 +1,41 @@
+///Task
+/*
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+
+Examples:
+
+Input -> Output
+[1, 1, 2] -> [1, 2]
+[1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+*/
+///Solution
+//My
+const distinct = d => [...new Set(d)];
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const { matchAll } = require('nunit-command/regex');
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant part describe
+describe("Sample Tests", () => {
+  it("test", () => {
+    assert.deepEqual(distinct([1]), [1]);
+    assert.deepEqual(distinct([1,2]), [1,2]);
+    assert.deepEqual(distinct([1,1,2]), [1,2]);
+  });
+});
