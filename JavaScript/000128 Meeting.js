96af1e790f78d80d43bdf73ab3eeb163ebfe0028
VillageR88 <villager88pl@gmail.com>
2023-09-19 18:18:04 +0200

    commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>
    
    commit
    
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ JavaScript/000128 Meeting.js
@@ -0,0 +1,63 @@
+///Task
+/*
+ohn has invited some friends. His list is:
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+
+Could you make a program that
+
+    makes this string uppercase
+    gives it sorted in alphabetical order by last name.
+
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+
+It can happen that in two distinct families with the same family name two people have the same first name too.
+Notes
+
+    You can see another examples in the "Sample tests".
+*/
+///Solution
+//My
+const meeting = s => s.split(";").map((x) => `(${x.toUpperCase().match(/(?<=:).+/g)}, ${x.toUpperCase().match(/.+(?=:)/g)})`).sort().join("");
+//Codewars recommended
+/*
+*/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const { matchAll } = require('nunit-command/regex');
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant part describe
+function testing(s, exp) {
+  console.log("Testing:\n", s)
+  let ans = meeting(s)
+  console.log("Actual:\n", ans)
+  console.log("Expect:\n", exp)
+  Test.assertEquals(ans, exp)
+}
+
+describe("Basic tests", function () {
+  it("meeting", function () {
+    testing("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn",
+      "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)");
+    testing("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell",
+      "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)");
+    testing("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern",
+      "(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)");
+
+  })
+})
