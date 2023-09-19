61fa06e55289cf84481b6cfd50ad89d7fba0a6f6
VillageR88 <villager88pl@gmail.com>
2023-09-19 22:47:05 +0200

    commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ JavaScript/000133. Kata Example Twist.js
@@ -0,0 +1,32 @@
+///Task
+/*
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.
+*/
+///Solution
+//My
+var websites = new Array(1000);
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const assert = chai.assert;
+const { matchAll } = require('nunit-command/regex');
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant part describe
+describe("Tests", () => {
+  it("test", () => {
+Test.expect(websites.length > 0, 'The array is still empty')
+Test.expect(websites.length == 1000, 'The array does not equal 1,000')
+Test.expect(websites.every(function(e){return e == 'codewars';}), 'Every element in the array must contain the value "codewars"');
+  });
+});
