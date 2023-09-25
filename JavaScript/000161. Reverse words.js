aa3cc6c49cb4f358e9df8142901c9640af72432e
VillageR88 <villager88pl@gmail.com>
2023-09-25 23:18:25 +0200

    commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ JavaScript/000161. Reverse words.js
@@ -0,0 +1,38 @@
+///Task
+/*
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+Examples
+
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+///Solution
+//My
+const reverseWords = s => s.split(" ").map((x) => [...x].reverse().join("")).join(" ");
+//Codewars recommended 
+/**/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const mocha = require('mocha');
+const assert = chai.assert;
+const { matchAll } = require('nunit-command/regex');
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant part describe
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+    assert.strictEqual(reverseWords('apple'), 'elppa');
+    assert.strictEqual(reverseWords('a b c d'), 'a b c d');
+    assert.strictEqual(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
+  });
+});
