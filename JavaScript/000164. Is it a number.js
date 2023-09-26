cb322e2e40c8cde09c2e96f8e288bce3536ddbf3
VillageR88 <villager88pl@gmail.com>
2023-09-26 22:04:27 +0200

    commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ JavaScript/000164. Is it a number.js
@@ -0,0 +1,48 @@
+///Task
+/*
+Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+Valid examples, should return true:
+
+isDigit("3")
+isDigit("  3  ")
+isDigit("-3.23")
+
+should return false:
+
+isDigit("3-4")
+isDigit("  3   5")
+isDigit("3 5")
+isDigit("zero")
+*/
+///Solution
+//My
+const isDigit = s => (s == " " || s == "") ? false : (s > -10 || s < 10) || typeof s == "float";
+//Codewars recommended 
+/**/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const mocha = require('mocha');
+const assert = chai.assert;
+const { matchAll } = require('nunit-command/regex');
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant part describe
+describe("Solution", function () {
+  it("basic tests", function () {
+    var input = "s2324";
+    //Test.assertEquals(isDigit(input), false);
+
+    input = "-234.4";
+    Test.assertEquals(isDigit(input), true);
+  });
+});
