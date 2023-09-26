70ee7817c21aefd5298f05b36d1d6786b850dbea
VillageR88 <villager88pl@gmail.com>
2023-09-26 17:04:45 +0200

    commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ JavaScript/000162. Sort the Gift Code.js
@@ -0,0 +1,45 @@
+///Task
+/*
+Happy Holidays fellow Code Warriors!
+
+Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.
+
+Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+Sort the Gift Code
+
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+Examples (Input -- => Output):
+
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+*/
+///Solution
+//My
+const sortGiftCode = w => [...(new Set(w))].sort().join("");
+//Codewars recommended 
+/**/
+///Tester => JS 'Codewars' 'Sample Test' Tester
+//Declarations and definitions - part adjusted to VSC
+const Test = require('@codewars/test-compat');
+const chai = require("chai");
+const mocha = require('mocha');
+const assert = chai.assert;
+const { matchAll } = require('nunit-command/regex');
+const deeepEqual = chai.assert.deepEqual;
+const strictEqual = require('chai').assert.strictEqual;
+chai.config.truncateThreshold = 0;
+function describe(tag, func) {
+  func();
+};
+function it(tag, func) {
+  func();
+};
+//Describe - invariant part describe
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(sortGiftCode('abcdef'), 'abcdef');
+    Test.assertEquals(sortGiftCode('pqksuvy'), 'kpqsuvy');
+    Test.assertEquals(sortGiftCode('zyxwvutsrqponmlkjihgfedcba'), 'abcdefghijklmnopqrstuvwxyz');
+  });
+});
