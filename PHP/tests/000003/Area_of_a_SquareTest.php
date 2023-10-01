0f187a7e06ec4c4279f0619efec90ae69468658d
VillageR88 <villager88pl@gmail.com>
2023-10-01 15:31:16 +0200

    commit
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- PHP/tests/000003/Area_of_a_SquareTest.php
+++ PHP/tests/000003/Area_of_a_SquareTest.php
@@ -9,18 +9,21 @@ Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
 */
 ///Solution
 //My
-function square_area($A): float {
-    return 0.00; // Change this
-  }
+function square_area($A): float
+{
+    return round((($A * 4) / (2 * M_PI)) ** 2, 2);
+}
 //Codewars recommended
 /**/
 ///Tester => PHP 'Codewars' 'Sample Test' Tester
 use PHPUnit\Framework\TestCase;
 
-class Area_of_a_SquareTest extends TestCase {
-    public function testFixed() {
-      $this->assertSame(0.00, square_area(0));
-      $this->assertSame(1.62, square_area(2));
-      $this->assertSame(80.00, square_area(14.05));
+class Area_of_a_SquareTest extends TestCase
+{
+    public function testFixed()
+    {
+        $this->assertSame(0.00, square_area(0));
+        $this->assertSame(1.62, square_area(2));
+        $this->assertSame(80.00, square_area(14.05));
     }
-  }
+}
