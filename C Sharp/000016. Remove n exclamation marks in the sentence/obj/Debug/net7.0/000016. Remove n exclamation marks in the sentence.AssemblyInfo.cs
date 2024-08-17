f2b42cd8274a92d15cbc501c460468fe24535a9e
VillageR88 <villager88pl@gmail.com>
2024-08-17 14:12:38 +0200

    gitignore
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- C Sharp/000016. Remove n exclamation marks in the sentence/obj/Debug/net7.0/000016. Remove n exclamation marks in the sentence.AssemblyInfo.cs
+++ C Sharp/000016. Remove n exclamation marks in the sentence/obj/Debug/net7.0/000016. Remove n exclamation marks in the sentence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("000016. Remove n exclamation marks in the sentence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91db859a5803911ad62ce9f3ec1c27aca9d51ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("000016. Remove n exclamation marks in the sentence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("000016. Remove n exclamation marks in the sentence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
