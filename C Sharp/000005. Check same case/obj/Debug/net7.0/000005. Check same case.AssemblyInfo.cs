265bd79a7621db033f54386849ec40a37078e444
VillageR88 <villager88pl@gmail.com>
2024-08-17 14:16:32 +0200

    rep update
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- C Sharp/000005. Check same case/obj/Debug/net7.0/000005. Check same case.AssemblyInfo.cs
+++ C Sharp/000005. Check same case/obj/Debug/net7.0/000005. Check same case.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("000005. Check same case")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d30c8f66ee59f0670f73d580024e22d80c6a4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("000005. Check same case")]
 [assembly: System.Reflection.AssemblyTitleAttribute("000005. Check same case")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C Sharp/000010. The 'if' function/obj/Debug/net7.0/000010. The 'if' function.AssemblyInfo.cs
+++ C Sharp/000010. The 'if' function/obj/Debug/net7.0/000010. The 'if' function.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("000010. The \'if\' function")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a91db859a5803911ad62ce9f3ec1c27aca9d51ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d30c8f66ee59f0670f73d580024e22d80c6a4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("000010. The \'if\' function")]
 [assembly: System.Reflection.AssemblyTitleAttribute("000010. The \'if\' function")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
